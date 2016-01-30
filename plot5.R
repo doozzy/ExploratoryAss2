72aef22df6f40eedafbf8555187ed6012f12c773
doozzy <oldoozd@gmail.com>
2016-01-30 15:45:26 +1100

    plot5 is completed

--- plot5.R
+++ plot5.R
@@ -1,12 +1,21 @@
 library(ggplot2)
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
-SCC[which(SCC$SCC.Level.One=="Mobile Sources"),]
+
+#find  motor vehicle sources code
 MotorSCC <- SCC[which(SCC$SCC.Level.One=="Mobile Sources"),]$SCC
+
+# find  motor vehicle sources emission
 both <- intersect(NEI$SCC, MotorSCC)
+
+# for Baltimore City 
 SubNEI <- subset(NEI, fips == "24510" & SCC %in% both)
 totalEmission <- tapply(SubNEI$Emissions, SubNEI$year, FUN = mean)
-plott= qplot(names(totalEmission), log10(totalEmission))
-print(plott)
+g<-  qplot(names(totalEmission), log10(totalEmission))
+g <- g + geom_line(aes(group = 1))
+g <- g + labs( x = "Year", y = "Emissions PM2.5  from motor vehicle sources")
+g<- g + labs(title = "Emissions PM2.5  from motor vehicle sources in \n 
+Baltimore City")
+print(g)
 dev.copy(png, file ="Plot5.png")
 dev.off()
