57dd2eb9b157d2dedd0c73a3eaa6ef5194103686
doozzy <oldoozd@gmail.com>
2016-01-30 15:06:52 +1100

    plot3 title fixed

--- plot3.R
+++ plot3.R
@@ -2,18 +2,23 @@ library(ggplot2)
 #read data
 NEI <- readRDS("./summarySCC_PM25.rds")
 SCC <- readRDS("./Source_Classification_Code.rds")
+
 #make two variables categorial
 NEI <- transform(NEI, year= factor(year))
 NEI <- transform(NEI, type= factor(type))
+
 #data for Baltimore city
 SubNEI <- subset(NEI, fips == "24510" )
+
 #aggregate Emission for each year and type 
 SubNEI <-  aggregate(Emissions ~ year * type,data=SubNEI, FUN=sum)
+
 #draw the graph
  g <- ggplot(SubNEI, aes(year, log10(Emissions), col = type),is.na = TRUE)
  g <- g + geom_point(alpha = 1/3) +facet_grid(. ~ type)+geom_line(aes(group=type))
  g<- g+ theme(axis.text.x = element_text(angle = 90, hjust = 1)) 
- g <- g + scale_x_discrete(breaks = seq(1999,2008,3)) 
+ g <- g + scale_x_discrete(breaks = seq(1999,2008,3))
+g <- g + labs(title = "Emission PM2.5 from four sources in Baltimore City") 
 print(g)
 dev.copy(png, file ="Plot3.png")
 dev.off()
