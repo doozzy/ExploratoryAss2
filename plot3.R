79a8e7459b6666a2387cc9334e189b3d85eaf28a
doozzy <oldoozd@gmail.com>
2016-01-30 14:26:53 +1100

    plot3 completed

--- plot3.R
+++ plot3.R
@@ -1,13 +1,19 @@
 library(ggplot2)
-NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
+#read data
+NEI <- readRDS("../summarySCC_PM25.rds")
+SCC <- readRDS("../Source_Classification_Code.rds")
+#make two variables categorial
 NEI <- transform(NEI, year= factor(year))
 NEI <- transform(NEI, type= factor(type))
+#data for Baltimore city
 SubNEI <- subset(NEI, fips == "24510" )
+#aggregate Emission for each year and type 
 SubNEI <-  aggregate(Emissions ~ year * type,data=SubNEI, FUN=sum)
-#qplot(log10(Emissions), year, data = SubNEI, facets=.~type, binwith=2,alpha =1/4)
-g <- ggplot(SubNEI, aes(log10(Emissions), year, fill = type),is.na = TRUE)
-g <- g + geom_point(alpha = 1/3) +geom_bar(stat="identity")+facet_grid(. ~ type) 
+#draw the graph
+ g <- ggplot(SubNEI, aes(year, log10(Emissions), col = type),is.na = TRUE)
+ g <- g + geom_point(alpha = 1/3) +facet_grid(. ~ type)+geom_line(aes(group=type))
+ g<- g+ theme(axis.text.x = element_text(angle = 90, hjust = 1)) 
+ g <- g + scale_x_discrete(breaks = seq(1999,2008,3)) 
 print(g)
 dev.copy(png, file ="Plot3.png")
 dev.off()
